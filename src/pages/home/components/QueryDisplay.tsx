244a8d6eff3cb03ab0b971807d85cfeb69862343
Mainasara Tsowa Al-amin <neutrino2211@gmail.com>
2023-10-25 16:28:15 +0100

    Only related questions should have typewriter effect

--- src/pages/home/components/QueryDisplay.tsx
+++ src/pages/home/components/QueryDisplay.tsx
@@ -37,7 +37,7 @@ function QueryDisplay({
         questionClick(id, question)
     }
 
-    const text = loadingId === 'loading' ? <i>Fetching response...</i> : <TypewriterComponent options={{ strings: docText, autoStart: true, delay: 5, loop: false }} />
+    const text = loadingId === 'loading' ? <i>Fetching response...</i> : <p>{docText}</p>
   
 
     return (
